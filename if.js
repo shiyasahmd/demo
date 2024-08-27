ed975293839b310420ffbc8cddab773bcfb5f174
shiyasahmd <119478366+shiyasahmd@users.noreply.github.com>
2024-08-27 10:18:17 +0530

    Add files via upload

---
+++ if.js
@@ -0,0 +1,114 @@
+// let temperature = 0;
+// if (temperature > 85) {
+//     console.log(" its hot outside");
+// } else if (temperature < 65){
+//     console.log("the weather is nice");
+// } else {
+//     console.log("weather is chilly");
+// } if 
+
+
+
+// switch statement
+
+// let plan = "basic";
+// switch (plan) {
+//     case "basic":
+//         console.log("basic plan selected");
+//         break;
+//     case "standard":
+//         console.log("standard plan selected");
+//         break;
+//     case "premium":
+//         console.log("premium plan selected");
+//         break;
+//     default:
+//         console.log("invalid plan")           
+// }
+  
+
+// for loop
+
+// let numbers = [ 1, 2, 3, 4, 5];
+// for (let i = 0; i < numbers.length; i++ ) {
+//     console.log(numbers[i]);
+// } 
+
+
+// for of
+
+// for (let num of numbers){
+//     console.log(num);
+// }
+ // let p1 = ["pen","pencil","eraser"];
+// for (let p2  of p1){
+//     console.log(p2);
+// // }
+
+
+//for in
+
+// let user ={ name :"Alice" , age : 25 , city: "new york"};
+// for (let key in user){
+//     console.log(`${key}:${user[key]}`)
+
+// }
+
+//while
+
+
+// let number = -1;
+// while (number  <= 0) {
+//     number = parseInt(prompt("please  enter a  positive number:"));
+//     if(number <= 0){
+//         console.log("that is not a positive number");
+//     }
+// }
+// console.log(`you have entered a positive number : ${number}`);
+
+//do while
+  
+// let num = 1;
+// do{
+//     console.log(num);
+//     num++
+
+// }while(num <= 5);
+// console.log("counting completed");  
+
+// arrow function
+
+// function add(a, b){
+//     return a+b;
+// }
+
+// const add = (a, b) => a+b;
+
+
+// let dog = (a, b) => a + b;
+// console.log(dog(3, 7));
+
+
+
+// let myr = () =>console.log("thyr");
+// myr();
+
+
+// let manuandi = (a, b) => {
+//     let result = a * b;
+//     return result;
+// };
+// console.log(manuandi(4, 5));
+
+// for (let i of orderItem){
+//     console.log(i);
+// }
+ 
+
+
+// //foreach
+
+// let kunn= ["chor","curry","upperi","achar","pappadam"];
+// kunn.forEach(item => {
+//     console.log(item);
+// });
